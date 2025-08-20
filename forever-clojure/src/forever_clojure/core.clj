1ad0dbb138db8c7b59cbcaf42e1a869277422806
Hans-J. Schmid <knock@myopendoor.de>
2025-08-20 03:38:04 +0200

    elementary

--- forever-clojure/src/forever_clojure/core.clj
+++ forever-clojure/src/forever_clojure/core.clj
@@ -135,8 +135,21 @@
 ;; 033 - Replicate a Sequence (easy)  
 ;; 034 - Implement range (easy)  
 ;; 035 - Local bindings (elementary)  
+
+(= 7 (let [x 5] (+ 2 x)))       ; true
+(= 7 (let [x 3, y 10] (- y x))) ; true
+(= 7 (let [x 21 y 3] (/ x y)))  ; true
+
 ;; 036 - Let it Be (elementary)  
+
+(= 10 (let [x 6, y 4] (+ x y))) ; true
+(= 4 (let [y 1, z 3] (+ y z)))  ; true
+(= 1 (let [z 1] z))             ; true
+
 ;; 037 - Regular Expressions (elementary)  
+
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce "))) ; true
+
 ;; 038 - Maximum value (easy)  
 ;; 039 - Interleave Two Seqs (easy)  
 ;; 040 - Interpose a Seq (easy)  
@@ -152,11 +165,17 @@
 ;; 050 - Split by Type (medium)  
 ;; 051 - Advanced Destructuring (easy)  
 ;; 052 - Intro to Destructuring (elementary)  
+
+(= [2 4] (let [[_a _b c _d e _f _g] (range)] [c e])) ; true
+
 ;; 053 - Longest Increasing SubSeq (hard)  
 ;; 054 - Partition a Sequence (medium)  
 ;; 055 - Count Occurences (medium)  
 ;; 056 - Find Distinct Items (medium)  
 ;; 057 - Simple Recursion (elementary)  
+
+(= '(5 4 3 2 1) ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)) ; true
+
 ;; 058 - Function Composition (medium)  
 ;; 059 - Juxtaposition (medium)  
 ;; 060 - Sequence Reductions (medium)  
@@ -164,14 +183,39 @@
 ;; 062 - Reimplement Iteration (easy)  
 ;; 063 - Group a Sequence (easy)  
 ;; 064 - Intro to Reduce (elementary)  
+
+(= 15 (reduce + [1 2 3 4 5])) ; true
+(=  0 (reduce + []))          ; true
+(=  6 (reduce + 1 [2 3]))     ; true
+
 ;; 065 - Black Box Testing (medium)  
 ;; 066 - Greatest Common Divisor (easy)  
 ;; 067 - Prime Numbers (medium)  
 ;; 068 - Recurring Theme (elementary)  
+
+(= [7 6 5 4 3]
+   (loop [x      5
+          result []]
+     (if (> x 0)
+       (recur (dec x) (conj result (+ 2 x)))
+       result)))
+
 ;; 069 - Merge with a Function (medium)  
 ;; 070 - Word Sorting (medium)  
 ;; 071 - Rearranging Code: > (elementary)  
+
+(= (count (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] reverse rest sort count)
+   5)
+; true
+
 ;; 072 - Rearranging Code: >> (elementary)  
+
+(= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce +))
+   11)
+; true
+
 ;; 073 - Analyze a TicTacToe Board (hard)  
 ;; 074 - Filter Perfect Squares (medium)  
 ;; 075 - Euler's Totient Function (medium)  
@@ -228,6 +272,16 @@
 ;; 131 - Sum Some Set Subsets (medium)  
 ;; 132 - Intervals (medium)  
 ;; 134 - A nil key (elementary)  
+
+(:a {:a nil :b 2})       ; nil
+(:b {:a nil :b 2})       ; 2
+(:c {:a nil :b 2})       ; nil
+(:c {:a nil :b 2} false) ; false
+
+(true?  (#(nil? (%1 %2 false)) :a {:a nil :b 2})) ; true
+(false? (#(nil? (%1 %2 false)) :b {:a nil :b 2})) ; true
+(false? (#(nil? (%1 %2 false)) :c {:a nil :b 2})) ; true
+
 ;; 135 - Infix Calculator (easy)  
 ;; 137 - Digits and bases (medium)  
 ;; 140 - Veitch, Please! (hard)  
@@ -235,16 +289,56 @@
 ;; 143 - dot product (easy)  
 ;; 144 - Oscilrate (medium)  
 ;; 145 - For the win (elementary)  
+
+(= '(1 5 9 13 17 21 25 29 33 37) (for [x (range 40)
+                                       :when (= 1 (rem x 4))]
+                                   x))
+; true
+
+(= '(1 5 9 13 17 21 25 29 33 37) (for [x (iterate #(+ 4 %) 0)
+                                       :let [z (inc x)]
+                                       :while (< z 40)]
+                                   z))
+; true
+
+(= '(1 5 9 13 17 21 25 29 33 37) (for [[x y] (partition 2 (range 20))]
+                                   (+ x y)))
+; true
+
 ;; 146 - Trees into tables (easy)  
 ;; 147 - Pascal's Trapezoid (easy)  
 ;; 148 - The Big Divide (medium)  
 ;; 150 - Palindromic Numbers (medium)  
 ;; 153 - Pairwise Disjoint Sets (easy)  
 ;; 156 - Map Defaults (elementary)  
+
+(zipmap [:a :b :c] (repeat 0)) ; {:a 0, :b 0, :c 0}
+
+(= (#(zipmap %2 (repeat %1)) 0 [:a :b :c]) {:a 0 :b 0 :c 0})                   ; true
+(= (#(zipmap %2 (repeat %1)) "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})                 ; true
+(= (#(zipmap %2 (repeat %1)) [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]}) ; true
+
 ;; 157 - Indexing Sequences (easy)  
 ;; 158 - Decurry (medium)  
 ;; 161 - Subset and Superset (elementary)  
+
+(clojure.set/superset? #{2}   #{2})   ; true
+(clojure.set/subset?   #{1}   #{1})   ; true
+(clojure.set/superset? #{1 2} #{1 2}) ; true
+(clojure.set/subset?   #{1 2} #{1 2}) ; true
+
 ;; 162 - Logical falsity and truth (elementary)  
+
+;; In Clojure, only `nil` and `false` represent the values of logical falsity in conditional tests - anything else is logical truth!!!
+
+(= 1 (if-not false 1 0)) ; true
+(= 1 (if-not nil 1 0))   ; true
+(= 1 (if true 1 0))      ; true
+(= 1 (if [] 1 0))        ; true
+(= 1 (if [0] 1 0))       ; true
+(= 1 (if 0 1 0))         ; true
+(= 1 (if 1 1 0))         ; true
+
 ;; 166 - Comparisons (easy)  
 ;; 168 - Infinite Matrix (medium)  
 ;; 171 - Intervals (medium)  
